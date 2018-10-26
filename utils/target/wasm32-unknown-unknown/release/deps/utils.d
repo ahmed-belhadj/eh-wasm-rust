34374d7c15bebae62e979bdd12e7f7aedce33ee7
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-10-26 17:09:48 -0500

    Load a WebAssembly Function Written in Rust and Invoke it from JavaScript

---
+++ utils/target/wasm32-unknown-unknown/release/deps/utils.d
@@ -0,0 +1,5 @@
+/Users/ahmedbelhadj/Documents/GitHub/Egghead/eh-wasm-rust/utils/target/wasm32-unknown-unknown/release/deps/utils.wasm: src/lib.rs
+
+/Users/ahmedbelhadj/Documents/GitHub/Egghead/eh-wasm-rust/utils/target/wasm32-unknown-unknown/release/deps/utils.d: src/lib.rs
+
+src/lib.rs:
---
+++ utils/target/wasm32-unknown-unknown/release/utils.d
@@ -0,0 +1,1 @@
+/Users/ahmedbelhadj/Documents/GitHub/Egghead/eh-wasm-rust/utils/target/wasm32-unknown-unknown/release/utils.wasm: /Users/ahmedbelhadj/Documents/GitHub/Egghead/eh-wasm-rust/utils/src/lib.rs
