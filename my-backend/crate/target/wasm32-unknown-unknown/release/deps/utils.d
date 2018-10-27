d0d35803ee56f9abe13676a111dadebfff6fae1a
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-10-27 01:31:17 -0500

    Run WebAssembly in Node.js using the node-loader

---
+++ my-backend/crate/target/wasm32-unknown-unknown/release/deps/utils.d
@@ -0,0 +1,5 @@
+/Users/ahmedbelhadj/Documents/GitHub/Egghead/eh-wasm-rust/my-backend/crate/target/wasm32-unknown-unknown/release/deps/utils.wasm: src/lib.rs
+
+/Users/ahmedbelhadj/Documents/GitHub/Egghead/eh-wasm-rust/my-backend/crate/target/wasm32-unknown-unknown/release/deps/utils.d: src/lib.rs
+
+src/lib.rs:
---
+++ my-backend/crate/target/wasm32-unknown-unknown/release/utils.d
@@ -0,0 +1,1 @@
+/Users/ahmedbelhadj/Documents/GitHub/Egghead/eh-wasm-rust/my-backend/crate/target/wasm32-unknown-unknown/release/utils.wasm: /Users/ahmedbelhadj/Documents/GitHub/Egghead/eh-wasm-rust/my-backend/crate/src/lib.rs
